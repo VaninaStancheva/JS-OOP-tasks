e75c5f9e4285e260ea5dea0811f793783f7b1873
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2024-08-22 16:15:37 +0300

    first commit

---
+++ simpleBookClass.js
@@ -0,0 +1,18 @@
+class Book {
+    constructor (title, author, yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    getSummary(book) {
+        const summery = `${this.title}, ${this.author}, ${this.yearPublished}`;
+        console.log(summery)
+    }
+}
+
+const bookOne = new Book ('FirstBook', 'First Author', '2024');
+const bookTwo = new Book ('SecondBook', 'Second Author', '2011');
+
+bookOne.getSummary();
+bookTwo.getSummary();
