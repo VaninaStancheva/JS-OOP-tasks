3a971d41728312db788a953908553bb2464461bf
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2025-01-13 10:48:17 +0200

    adding savings

--- encapsulation/02-bank-account.js
+++ encapsulation/02-bank-account.js
@@ -3,16 +3,19 @@ class BankAccount {
     // Private properties for balance and account number.
     #balance
     #accountNumber
+    #savings
     /**
      * Initialize the BankAccount instance with balance, account number, and user number.
      * @param {number} balance - The initial balance of the account in Euros.
      * @param {number} accountNumber - The unique account number.
      * @param {string} userNumber - An additional user-defined identifier.
+     * @param {number} savings - The initial savings of the account in Euros
      */
-    constructor (balance, accountNumber, userNumber) {
+    constructor (balance, accountNumber, userNumber, savings = 0) {
         this.#accountNumber = accountNumber; // Assign account number to the private #accountNumber property.
         this.#balance = balance;            // Assign balance to the private #balance property in Euros.
         this.userNumber = userNumber;      // Assign string to a public property. ('u-accountNumber')
+        this.#savings = savings;
     }
 
     get user () {
@@ -54,12 +57,25 @@ class BankAccount {
     getBalance() {
         return this.#balance
     }
+    /**
+     * Retrieve the current savings of the account in Euros.
+     * @returns {number} The current savings of the account.
+     */
+    get savings() {
+        if (this.#balance > 100) {
+            return this.#savings += this.#balance * 0.2;
+        } else {
+            console.log('Not enough money in account to be saved.')
+        }
+    }
 }
 
-const bankAccount = new BankAccount(200, 594289, 'u-594289');
+const bankAccount = new BankAccount(600, 594289, 'u-594289');
 
 console.log(bankAccount.deposit(100));
 console.log(bankAccount.withdraw(50));
 console.log(bankAccount.getBalance());
 console.log(bankAccount.withdraw(260));
 console.log(bankAccount.getBalance());
+console.log(bankAccount.savings);
+
